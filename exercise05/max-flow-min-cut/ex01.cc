733da6af7f21bba5f72985583248410fee563f8b
pflegusch <dennis.pfleger@web.de>
2022-05-29 14:17:53 +0200

    Submission 1 - 19/20p

--- exercise05/max-flow-min-cut/ex01.cc
+++ exercise05/max-flow-min-cut/ex01.cc
@@ -1,51 +1,185 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <time.h>
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void addEdge(pair<int, vector<pair<int, int>>> adj[], int u, int v, int wt) {
-    adj[u].second.push_back(make_pair(v, wt));
-    adj[v].second.push_back(make_pair(u, wt));
-}
+struct Edge
+{
+    int v;
+    int flow;
+    int C;
+    int rev;
+};
 
-void printGraph(pair<int, pair<int, vector<pair<int, int>>>> adj[], int V) {
-    int v, w;
-    for (int u = 1; u <= V; u++) {
-        cout << "Node " << u << " with weight " << adj[u].second.first << 
-        " and degree " << adj[u].first << " makes an edge with \n";
-        for (auto it = adj[u].second.second.begin(); it != adj[u].second.second.end(); it++) {
-            v = it->first;
-            w = it->second;
-            cout << "\tNode " << v << " (w = " << adj[v].second.first << ", d = " << adj[v].first << ") with edge weight = "
-                 << w << "\n";
-        }
-        cout << "\n";
+class Graph
+{
+    int V;
+    vector<Edge> *adj;
+
+public:
+    int *level;
+    Graph(int V)
+    {
+        adj = new vector<Edge>[V];
+        this->V = V;
+        level = new int[V];
     }
+
+    // add edge to the graph
+    void addEdge(int u, int v, int C)
+    {
+        // Forward edge : 0 flow and C capacity
+        Edge a{v, 0, C, adj[v].size()};
+
+        // Back edge : 0 flow and 0 capacity
+        Edge b{u, 0, 0, adj[u].size()};
+
+        adj[u].push_back(a);
+        adj[v].push_back(b); // reverse edge
+    }
+
+    bool BFS(int s, int t);
+    int sendFlow(int s, int flow, int t, int ptr[]);
+    void DinicMaxflow(int s, int t);
+};
+
+// Finds if more flow can be sent from s to t.
+// Also assigns levels to nodes.
+bool Graph::BFS(int s, int t)
+{
+    for (int i = 0; i < V; i++)
+        level[i] = -1;
+
+    level[0] = -2;
+    level[s] = 0; // Level of source vertex
+
+    // Create a queue, enqueue source vertex
+    // and mark source vertex as visited here
+    // level[] array works as visited array also.
+    list<int> q;
+    q.push_back(s);
+
+    vector<Edge>::iterator i;
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop_front();
+        for (i = adj[u].begin(); i != adj[u].end(); i++)
+        {
+            Edge &e = *i;
+            if (level[e.v] < 0 && e.flow < e.C)
+            {
+                // Level of current vertex is,
+                // level of parent + 1
+                level[e.v] = level[u] + 1;
+
+                q.push_back(e.v);
+            }
+        }
+    }
+
+    // IF we can not reach to the sink we
+    // return false else true
+    return level[t] < 0 ? false : true;
 }
 
-int main(int argc, char** argv) {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    int n, m, w;
+// A DFS based function to send flow after BFS has
+// figured out that there is a possible flow and
+// constructed levels. This function called multiple
+// times for a single call of BFS.
+// flow : Current flow send by parent function call
+// start[] : To keep track of next edge to be explored.
+//           start[i] stores  count of edges explored
+//           from i.
+//  u : Current vertex
+//  t : Sink
+int Graph::sendFlow(int u, int flow, int t, int start[])
+{
+    // Sink reached
+    if (u == t)
+        return flow;
+
+    // Traverse all adjacent edges one -by - one.
+    for (; start[u] < adj[u].size(); start[u]++)
+    {
+        // Pick next edge from adjacency list of u
+        Edge &e = adj[u][start[u]];
+
+        if (level[e.v] == level[u] + 1 && e.flow < e.C)
+        {
+            // find minimum flow from u to t
+            int curr_flow = min(flow, e.C - e.flow);
+
+            int temp_flow = sendFlow(e.v, curr_flow, t, start);
+
+            // flow is greater than zero
+            if (temp_flow > 0)
+            {
+                // add flow  to current edge
+                e.flow += temp_flow;
+
+                // subtract flow from reverse edge
+                // of current edge
+                adj[e.v][e.rev].flow -= temp_flow;
+                return temp_flow;
+            }
+        }
+    }
+
+    return 0;
+}
+
+// Returns maximum flow in graph
+void Graph::DinicMaxflow(int s, int t)
+{
+    // Corner case
+    // if (s == t)
+    //     return -1;
+
+    int total = 0; // Initialize result
+
+    // Augment the flow while there is path
+    // from source to sink
+    while (BFS(s, t))
+    {
+        // store how many edges are visited
+        // from V { 0 to V }
+        int *start = new int[V + 1]{0};
+
+        // while flow is not zero in graph from S to D
+        while (int flow = sendFlow(s, INT_MAX, t, start))
+
+            // Add path flow to overall flow
+            total += flow;
+    }
+
+    // return maximum flow
+    cout << total << endl;
+
+    // return set
+    for (int i = 1; i < V; i++)
+    {
+        if (level[i] != -1)
+            cout << i << " ";
+    }
+    cout << endl;
+}
+
+// Driver Code
+int main()
+{
+    int n, m, w, s, t;
     cin >> n >> m;
-    pair<int, vector<pair<int, int>>> adj[m];
-    
-    for (int i = 0; i < 2*m; i++) {
+    Graph g(n + 1);
+
+    for (int i = 0; i < 2 * m; i++)
+    {
         int src, dest, w;
         cin >> src >> dest >> w;
-        addEdge(adj, src, dest, w);
+        g.addEdge(src, dest, w);
     }
 
-    // Erase duplicates and sort adj after weights to always have the smallest one at the top
-    // Necessary for the above implementation to work
-    for (int u = 1; u <= n; u++) {
-        sort(adj[u].second.begin(), adj[u].second.end());
-        adj[u].second.erase(unique(adj[u].second.begin(), adj[u].second.end()), adj[u].second.end());
-    }
+    cin >> s >> t;
+    g.DinicMaxflow(s, t);
 
     return 0;
 }
