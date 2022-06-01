03189d180051eeea05aadaad99f42d5eaebfcacf
pflegusch <dennis.pfleger@web.de>
2022-06-01 21:04:21 +0200

    add g parameter for gdb and push code

--- exercise06/neg-cycle-detection/ex01.cc
+++ exercise06/neg-cycle-detection/ex01.cc
@@ -1,9 +1,6 @@
-#include <cmath>
-#include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <time.h>
 #include "bits/stdc++.h"
 
 using namespace std;
@@ -32,6 +29,83 @@ void printGraph(pair<int, vector<pair<int, int>>> adj[], int V)
             cout << "\tNode " << v << " with edge weight = " << w << "\n";
         }
         cout << "\n";
+    }
+}
+
+void solve(pair<int, vector<pair<int, int>>> adj[], int V, int E)
+{
+    vector<int> vis(V, 0);
+    int dist[V], parent[V];
+
+    // Step 1: Initialize distances from 1
+    // to all other vertices as INFINITE
+    for (int i = 0; i <= V; i++)
+    {
+        dist[i] = INT_MAX;
+        parent[i] = -1;
+    }
+    dist[1] = 0;
+    vis[1] = 0;
+
+    bool flag = true;
+    for (int i = 1; i <= V - 1; i++)
+    {
+        if (!flag)
+            break;
+        flag = false;
+        for (auto it = adj[i].second.begin(); it != adj[i].second.end(); it++)
+        {
+            int u = i;
+            int v = it->first;
+            int w = it->second;
+            if (dist[u] != INT_MAX && dist[u] + w < dist[v])
+            {
+                flag = true;
+                vis[v] = 1;
+                dist[v] = dist[u] + w;
+                parent[v] = u;
+            }
+        }
+    }
+
+    int c = -1;
+    for (int i = 1; i <= V; i++)
+    {
+        for (auto it = adj[i].second.begin(); it != adj[i].second.end(); it++)
+        {
+            int u = i;
+            int v = it->first;
+            int w = it->second;
+
+            if (dist[u] != INT_MAX && dist[u] + w < dist[v])
+            {
+                c = v;
+                break;
+            }
+        }
+    }
+
+    if (c != -1)
+    {
+        for (int i = 1; i <= V; i++)
+        {
+            cout << c << " ";
+            c = parent[c];
+        }
+
+        // vector<int> cycle;
+        // for (int v = c;; v = parent[v])
+        // {
+        //     cycle.push_back(v);
+        // if (v == c && cycle.size() > 1)
+        //     break;
+        // }
+        // reverse(cycle.begin(), cycle.end());
+        // for (int v : cycle)
+        // {
+        //     cout << v << " ";
+        // }
+        // cout << endl;
     }
 }
 
@@ -64,6 +138,7 @@ int main(int argc, char **argv)
     }
 
     printGraph(adj, n);
+    solve(adj, n, m);
 
     return 0;
 }
